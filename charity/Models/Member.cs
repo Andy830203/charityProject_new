14d0a5b6fa6b76a42987d53d4e0e037e21a8c151
Ted Sun <catchsun5305@gmail.com>
2024-09-14 04:01:55 +0800

    生成 Event Model 部分

---
+++ charity/Models/Member.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace charity.Models;
+
+public partial class Member
+{
+    public int Id { get; set; }
+
+    public string Account { get; set; } = null!;
+
+    public string Password { get; set; } = null!;
+
+    public string? NickName { get; set; }
+
+    public string? RealName { get; set; }
+
+    public bool? Gender { get; set; }
+
+    public DateTime? Birthday { get; set; }
+
+    public string? Email { get; set; }
+
+    public string? Address { get; set; }
+
+    public string? Phone { get; set; }
+
+    public int Points { get; set; }
+
+    public int Checkin { get; set; }
+
+    public int Exp { get; set; }
+
+    public string? ImgName { get; set; }
+
+    public int Status { get; set; }
+
+    public int Access { get; set; }
+
+    public bool? FaceRec { get; set; }
+
+    public virtual ICollection<Event> Events { get; set; } = new List<Event>();
+}
--- charity/Program.cs
+++ charity/Program.cs
@@ -1,4 +1,5 @@
 using charity.Data;
+using charity.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
@@ -9,6 +10,10 @@ var connectionString = builder.Configuration.GetConnectionString("DefaultConnect
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+builder.Services.AddDbContext<CharityContext>(
+    options => options.UseSqlServer(builder.Configuration.GetConnectionString("Charity"))
+    );
 
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddEntityFrameworkStores<ApplicationDbContext>();
