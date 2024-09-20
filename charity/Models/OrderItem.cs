cd7ae84d78e9829970d5513d01eac07faa23abdd
AndyLu <810376@gmail.com>
2024-09-20 10:45:53 +0800

    A Failed Version about CharityContext.

---
+++ charity/Models/OrderItem.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace charity.Models;
+
+public partial class OrderItem
+{
+    public int Id { get; set; }
+
+    public int OId { get; set; }
+
+    public int PId { get; set; }
+
+    public int Quantity { get; set; }
+
+    public DateTime? ShippedTime { get; set; }
+
+    public virtual Order OIdNavigation { get; set; } = null!;
+
+    public virtual Product PIdNavigation { get; set; } = null!;
+}
---
+++ charity/Models/SignUp.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace charity.Models;
+
+public partial class SignUp
+{
+    public int Id { get; set; }
+
+    public int EpId { get; set; }
+
+    public int Applicant { get; set; }
+
+    public virtual Member ApplicantNavigation { get; set; } = null!;
+
+    public virtual EventPeriod Ep { get; set; } = null!;
+}
