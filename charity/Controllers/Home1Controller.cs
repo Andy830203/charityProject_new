ccfcd059a1aa15a4d3d964f4876333a50165f4ad
steven <mistake123415@gmail.com>
2024-09-20 09:18:25 +0800

    實作上傳圖片至wwwroot

---
+++ charity/Controllers/Home1Controller.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using System.IO;
+using System.Threading.Tasks;
+
+public class Home1Controller : Controller
+{
+    //宣告了一個 _environment 變數，用來儲存當前應用程式的環境資訊（例如根目錄位置）。
+    private readonly IWebHostEnvironment _environment;
+    //接受並初始化 _environment
+    public Home1Controller(IWebHostEnvironment environment)
+    {
+        _environment = environment;
+    }
+   
+    // GET: Index
+    public IActionResult Index()
+    {
+        return View();
+    }
+    public IActionResult testupload()
+    {
+        return View();
+    }
+    // POST: UploadFile
+    //處理上傳檔案的 POST 請求方法，傳入一個 IFormFile 物件，表示使用者選擇上傳的檔案。
+    [HttpPost]
+    public async Task<IActionResult> UploadFile(IFormFile file)
+    {
+        //驗證檔案是否存在
+        if (file != null && file.Length > 0)
+        {
+            var filepath = "~/wwwroot/";
+            //將wwwroot(靜態資源根目錄)與test結合
+            var path = Path.Combine(filepath, "test");
+            //var path = Path.Combine(_environment.WebRootPath, "test");
+            // 確認 "test" 資料夾是否存在，若不存在則建立
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+            // 建立檔案的儲存路徑
+            var filePath = Path.Combine(path, file.FileName);
+
+            // 將檔案儲存至指定的資料夾
+            using (var stream = new FileStream(filePath, FileMode.Create))
+            {
+                await file.CopyToAsync(stream);
+            }
+
+            ViewBag.Message = "檔案上傳成功!";
+        }
+        else
+        {
+            ViewBag.Message = "請選擇一個檔案上傳!";
+        }
+        //返回 Index
+        return View("../Home/Index");
+    }
+}
---
+++ charity/Controllers/LocationImgsController.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using charity.Models;
+
+namespace charity.Controllers
+{
+    public class LocationImgsController : Controller
+    {
+        private readonly CharityContext _context;
+
+        public LocationImgsController(CharityContext context)
+        {
+            _context = context;
+        }
+
+        // GET: LocationImgs
+        public async Task<IActionResult> Index()
+        {
+            var charityContext = _context.LocationImgs.Include(l => l.LIdNavigation);
+            return View(await charityContext.ToListAsync());
+        }
+
+        // GET: LocationImgs/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var locationImg = await _context.LocationImgs
+                .Include(l => l.LIdNavigation)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (locationImg == null)
+            {
+                return NotFound();
+            }
+
+            return View(locationImg);
+        }
+
+        // GET: LocationImgs/Create
+        public IActionResult Create()
+        {
+            ViewData["LId"] = new SelectList(_context.Locations, "Id", "Id");
+            return View();
+        }
+
+        // POST: LocationImgs/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,LId,ImgName")] LocationImg locationImg)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(locationImg);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["LId"] = new SelectList(_context.Locations, "Id", "Id", locationImg.LId);
+            return View(locationImg);
+        }
+
+        // GET: LocationImgs/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var locationImg = await _context.LocationImgs.FindAsync(id);
+            if (locationImg == null)
+            {
+                return NotFound();
+            }
+            ViewData["LId"] = new SelectList(_context.Locations, "Id", "Id", locationImg.LId);
+            return View(locationImg);
+        }
+
+        // POST: LocationImgs/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,LId,ImgName")] LocationImg locationImg)
+        {
+            if (id != locationImg.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(locationImg);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!LocationImgExists(locationImg.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["LId"] = new SelectList(_context.Locations, "Id", "Id", locationImg.LId);
+            return View(locationImg);
+        }
+
+        // GET: LocationImgs/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var locationImg = await _context.LocationImgs
+                .Include(l => l.LIdNavigation)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (locationImg == null)
+            {
+                return NotFound();
+            }
+
+            return View(locationImg);
+        }
+
+        // POST: LocationImgs/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var locationImg = await _context.LocationImgs.FindAsync(id);
+            if (locationImg != null)
+            {
+                _context.LocationImgs.Remove(locationImg);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool LocationImgExists(int id)
+        {
+            return _context.LocationImgs.Any(e => e.Id == id);
+        }
+    }
+}
