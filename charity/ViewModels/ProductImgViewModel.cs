bf4b6ee333d95a1d27a538c0225de74b811cf067
AndyLu <810376@gmail.com>
2024-09-21 18:08:50 +0800

    create view新增圖片上傳，viewmodel新增一個屬性存上傳的檔案，controller還沒有修改

--- charity/ViewModels/ProductImgViewModel.cs
+++ charity/ViewModels/ProductImgViewModel.cs
@@ -3,6 +3,7 @@ using charity.Models;
 namespace charity.ViewModels {
     public class ProductImgViewModel {
         public Product product {  get; set; }
+        public List<IFormFile> UploadedImages { get; set; }  // 用來上傳多張圖片
         public List<string> productImgs { get; set; }
     }
 }
