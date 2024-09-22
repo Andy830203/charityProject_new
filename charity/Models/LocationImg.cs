edf4810bf9a653447afb8a927d629568c6694c29
steven <mistake123415@gmail.com>
2024-09-23 04:21:21 +0800

    修正按鈕詞彙

--- charity/Models/LocationImg.cs
+++ charity/Models/LocationImg.cs
@@ -1,15 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace charity.Models;
 
 public partial class LocationImg
 {
+    [Display(Name = "地點圖片id")]
     public int Id { get; set; }
-
+    [Display(Name = "地點id")]
     public int? LId { get; set; }
-
+    [Display(Name = "地點圖片名稱")]
     public string? ImgName { get; set; }
-
+    [Display(Name = "地點圖片位置")]
     public virtual Location? LIdNavigation { get; set; }
 }
