63a73fa09728f90d849ad0c7ca9b10c0f85e3804
Ted Sun <catchsun5305@gmail.com>
2024-09-24 16:15:43 +0800

    [中文]用Metadata更改顯示的語言 [其他]微調手風琴以及版面卷軸設定, 加入EventsViewModel

---
+++ charity/Metadatas/EventMetadata.cs
@@ -0,0 +1,43 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace charity.Models
+{
+    public class EventMetadata
+    {
+        [Display(Name = "活動ID")]
+        public int Id { get; set; }
+
+        [Display(Name = "活動名稱")]
+        public string? Name { get; set; }
+
+        public int? OrganizerId { get; set; }
+
+        [Display(Name = "報名費")]
+        public int? Fee { get; set; }
+
+        [Display(Name = "報名名額")]
+        public int? Capacity { get; set; }
+
+        [Display(Name = "活動描述")]
+        public string? Description { get; set; }
+
+        [Display(Name = "急迫性")]
+        public int? Priority { get; set; }
+
+        public int? CategoryId { get; set; }
+
+        [Display(Name = "活動種類")]
+        public virtual EventCategory? Category { get; set; }
+
+        public virtual ICollection<Comment> Comments { get; set; } = new List<Comment>();
+
+        public virtual ICollection<EventImg> EventImgs { get; set; } = new List<EventImg>();
+
+        public virtual ICollection<EventLocation> EventLocations { get; set; } = new List<EventLocation>();
+
+        public virtual ICollection<EventPeriod> EventPeriods { get; set; } = new List<EventPeriod>();
+
+        [Display(Name = "發起人")]
+        public virtual Member? Organizer { get; set; }
+    }
+}
