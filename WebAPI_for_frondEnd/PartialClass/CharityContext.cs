9be9d2402e08cacd18741ee847d96dcf557a1c78
Ren <s12111110@stu.edu.tw>
2024-10-21 17:47:28 +0800

    要還原的變更

---
+++ WebAPI_for_frondEnd/PartialClass/CharityContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebAPI_for_frondEnd.Models;
+
+namespace WebAPI_for_frondEnd.Models
+{
+    public partial class charityContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                IConfiguration config = new ConfigurationBuilder()
+                    .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+                    .AddJsonFile("appsettings.json")
+                    .Build();
+                optionsBuilder.UseSqlServer(config.GetConnectionString("charity"));
+            }
+        }
+    }
+}
