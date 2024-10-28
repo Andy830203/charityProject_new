b879bcaaf7c164d7ce3aea1f19e5d37befd200e7
AndyLu <810376@gmail.com>
2024-10-28 10:47:40 +0800

    product category controller生成，get all功能正常

---
+++ WebAPI_for_frondEnd/DTO/ProductCategoryDTO.cs
@@ -0,0 +1,7 @@
+﻿namespace WebAPI_for_frondEnd.DTO {
+    public class ProductCategoryDTO {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int amount { get; set; }
+    }
+}
