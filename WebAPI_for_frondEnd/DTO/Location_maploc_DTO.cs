4d2c78ce888b7197449581f52e959fa180fd353b
steven <mistake123415@gmail.com>
2024-11-08 11:10:30 +0800

    新增marker使用dto

---
+++ WebAPI_for_frondEnd/DTO/Location_maploc_DTO.cs
@@ -0,0 +1,11 @@
+﻿namespace WebAPI_for_frondEnd.DTO
+{
+    public class Location_maploc_DTO
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public decimal? Longitude { get; set; }
+        public decimal? Latitude { get; set; }
+        public string? Address { get; set; }
+    }
+}
